dbfc8214bde2769fbfa4d8b8b8bdb78b9edd6f30
tyagi26072004 <hrithiktyagi56@gmail.com>
2026-01-12 17:07:24 +0530

    add frontend portfolio files

---
+++ script.js
@@ -0,0 +1,29 @@
+const form=document.getElementById("contactForm")
+
+form.addEventListener("submit" , async(e)=>{
+    e.preventDefault();
+
+    const data ={
+        name: form.name.value,
+        email: form.email.value,
+        phone: form.phone.value,
+        service: form.service.value,
+        message: form.message.value, 
+    };
+    try{
+        const response = await
+        fetch("https://portfolio-backend-1-kpk8.onrender.com",{
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json",
+            },
+            body: JSON.stringify(data),
+        });
+        const result= await response.json();
+        alert(result.message);
+        form.reset()
+
+    }  catch (error) {
+        alert("Failed to send message")
+    }
+})
