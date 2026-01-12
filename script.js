20db75af64c2f4354b9b30948487fc434780503a
tyagi26072004 <hrithiktyagi56@gmail.com>
2026-01-12 22:48:35 +0530

    fix contact API endpoint

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ form.addEventListener("submit" , async(e)=>{
     };
     try{
         const response = await
-        fetch("https://portfolio-backend-1-kpk8.onrender.com",{
+        fetch("https://portfolio-backend-1-kpk8.onrender.com/contact",{
             method:"POST",
             headers:{
                 "Content-Type":"application/json",
